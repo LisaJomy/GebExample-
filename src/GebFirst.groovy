c878cb31bd8da84040fff965aedb5472b5fc9fde
Lisa Jomy <lisa.selenium@gmail.com>
2020-08-06 20:40:18 -0400

    This is first geb file

---
+++ src/GebFirst.groovy
@@ -0,0 +1,7 @@
+import geb.Browser
+import org.openqa.selenium.firefox.FirefoxDriver
+
+def browser =new Browser(driver : new FirefoxDriver())
+
+
+browser.go("http://udemy.com")
---
+++ src/LaunchChrome.groovy
@@ -0,0 +1,8 @@
+import geb.Browser
+import org.openqa.selenium.chrome.ChromeDriver
+
+
+def browser =new Browser(driver : new ChromeDriver())
+
+
+browser.go("http://udemy.com")
